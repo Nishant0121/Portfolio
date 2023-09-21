3815443ec275edf3d8fcf2eb8e16c08992e6b465
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2023-09-21 22:08:20 +0530

    Add files via upload
    java script added

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector(".nav");
+
+function changecolor() {
+    nav.style.color= "blue";
+}
+function showskills() {
+    const skills="A frontend web developer , Sketch Artist";
+    document.getElementById("displayskill").innerText=skills;
+    let showskills=document.getElementById("showskills");
+    showskills.style.display="none";
+    let hideskills=document.getElementById("hideskills");
+    hideskills.style.display="block";
+}
+function hideskills() {
+    let hideskills=document.getElementById("displayskill");
+    hideskills.style.display="none";
+    let showskills=document.getElementById("showskills");
+    showskills.style.display="block";
+}
